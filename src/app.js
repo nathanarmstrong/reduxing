45340e64493b0634f37353a0a5d2cebf428fb5b1
nathanarmstrong <nathan.armstrong21@gmail.com>
2018-02-21 14:18:07 -0700

    Redux react started pack

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+//components
+import MainComponent from "./components/Main.js";
+
+ReactDOM.render(
+                <MainComponent />
+                ,document.getElementById("main")
+                );
